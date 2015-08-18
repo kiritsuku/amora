b812e204da6939745d01a65e3fc734ab8620ee3b
Simon Schäfer <mail@antoras.de>
2015-08-18 16:53:53 +0200

    Add Nvim.sendInput command

--- nvim/src/main/scala/nvim/Nvim.scala
+++ nvim/src/main/scala/nvim/Nvim.scala
@@ -6,6 +6,7 @@ import scala.concurrent.Future
 import akka.actor.ActorSystem
 import msgpack4z.MsgpackArray
 import msgpack4z.MsgpackExt
+import msgpack4z.MsgpackLong
 import msgpack4z.MsgpackUnion
 import nvim.internal.NvimHelper
 
@@ -18,13 +19,29 @@ final case class Nvim(connection: Connection)(implicit val system: ActorSystem) 
   }
 
   /**
-   * Sends `cmd` to Nvim. Example usage:
+   * Sends `cmd` to Nvim. If the command does not exist, this function doesn't
+   * report back an error message. Example usage:
    * {{{
    * nvim.sendVimCommand(":help")
+   * // This is more or less equivalent to:
+   * nvim.sendInput("<esc>:help<cr>")
    * }}}
    */
   def sendVimCommand(cmd: String): Unit =
     connection.sendNotification("vim_command", MsgpackUnion.string(cmd))
+
+  /**
+   * Sends `input` to Nvim. If the request was successful, Nvim will answer with
+   * the string length of `input`. Example usage:
+   * {{{
+   * nvim.sendInput("<esc>ggAhello<esc>") // return value: 18
+   * }}}
+   */
+  def sendInput(input: String)(implicit ec: ExecutionContext): Future[Long] = {
+    connection.sendRequest("vim_input", MsgpackUnion.string(input)) {
+      case MsgpackLong(long) => long
+    }
+  }
 
   def buffers(implicit ec: ExecutionContext): Future[Seq[Buffer]] = {
     connection.sendRequest("vim_get_buffers") {
