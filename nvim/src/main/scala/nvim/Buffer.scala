d03bfd053a4de7e402bf9f84970d02ea435a79e4
Simon Schäfer <mail@antoras.de>
2015-08-18 19:25:04 +0200

    Add Buffer.{lineAt,lineSlice} functions

--- nvim/src/main/scala/nvim/Buffer.scala
+++ nvim/src/main/scala/nvim/Buffer.scala
@@ -4,23 +4,45 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
 import akka.actor.ActorSystem
+import msgpack4z.MsgpackArray
 import msgpack4z.MsgpackBinary
 import msgpack4z.MsgpackLong
-import msgpack4z.MsgpackUnion
+import msgpack4z.MsgpackUnion._
 
 final case class Buffer(id: Int, connection: Connection)(implicit system: ActorSystem) {
 
   def name(implicit ec: ExecutionContext): Future[String] = {
-    connection.sendRequest("buffer_get_name", msgId) {
-      case MsgpackBinary(bin) => new String(bin, "UTF-8")
+    connection.sendRequest("buffer_get_name", int(id)) {
+      case MsgpackBinary(bin) ⇒ new String(bin, "UTF-8")
     }
   }
 
-  def lineCount(implicit ec: ExecutionContext): Future[Long] = {
-    connection.sendRequest("buffer_line_count", msgId) {
-      case MsgpackLong(long) => long
+  def lineCount(implicit ec: ExecutionContext): Future[Int] = {
+    connection.sendRequest("buffer_line_count", int(id)) {
+      case MsgpackLong(long) ⇒ long.toInt
     }
   }
 
-  private def msgId = MsgpackUnion.int(id)
+  def lineAt(index: Int)(implicit ec: ExecutionContext): Future[String] = {
+    connection.sendRequest("buffer_get_line", int(id), int(index)) {
+      case MsgpackBinary(bin) ⇒ new String(bin, "UTF-8")
+    }
+  }
+
+  def lineSlice
+      (start: Int, end: Int, includeStart: Boolean = true, includeEnd: Boolean = false)
+      (implicit ec: ExecutionContext)
+      : Future[Seq[String]] = {
+    connection.sendRequest("buffer_get_line_slice", int(id), int(start), int(end), bool(includeStart), bool(includeEnd)) {
+      case MsgpackArray(lines) ⇒
+        if (!lines.forall(_.isInstanceOf[MsgpackBinary]))
+          throw new UnexpectedResponse(lines.toString)
+        else
+          lines map { x => (x: @unchecked) match {
+            case MsgpackBinary(bin) ⇒
+              new String(bin, "UTF-8")
+          }}
+    }
+  }
+
 }
