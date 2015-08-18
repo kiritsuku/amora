d00d91ab0ccb7db7cbe33b2d82dc468ec6b99a5d
Simon Schäfer <mail@antoras.de>
2015-08-18 18:52:44 +0200

    Add Buffer.lineCount

--- nvim/src/main/scala/nvim/Buffer.scala
+++ nvim/src/main/scala/nvim/Buffer.scala
@@ -2,19 +2,25 @@ package nvim
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
-import scala.util.Failure
-import scala.util.Success
 
 import akka.actor.ActorSystem
 import msgpack4z.MsgpackBinary
+import msgpack4z.MsgpackLong
 import msgpack4z.MsgpackUnion
 
 final case class Buffer(id: Int, connection: Connection)(implicit system: ActorSystem) {
 
   def name(implicit ec: ExecutionContext): Future[String] = {
-    connection.sendRequest("buffer_get_name", MsgpackUnion.int(id)) {
-      case MsgpackBinary(bin) =>
-        new String(bin, "UTF-8")
+    connection.sendRequest("buffer_get_name", msgId) {
+      case MsgpackBinary(bin) => new String(bin, "UTF-8")
     }
   }
+
+  def lineCount(implicit ec: ExecutionContext): Future[Long] = {
+    connection.sendRequest("buffer_line_count", msgId) {
+      case MsgpackLong(long) => long
+    }
+  }
+
+  private def msgId = MsgpackUnion.int(id)
 }
