e80a6014516dc3367ecc06bbd73ebde41ca6406e
Simon Schäfer <mail@antoras.de>
2015-08-21 19:44:17 +0200

    Implement Window.cursor

--- nvim/src/main/scala/nvim/Window.scala
+++ nvim/src/main/scala/nvim/Window.scala
@@ -1,7 +1,23 @@
 package nvim
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
 import akka.actor.ActorSystem
+import msgpack4z._
+import msgpack4z.MsgpackUnion._
 
 case class Window(id: Int, connection: Connection)(implicit system: ActorSystem) {
+
+  /**
+   * Returns the cursor of the window as a tuple of row and column. The row
+   * starts at 1 and the column starts at 0.
+   */
+  def cursor(implicit ec: ExecutionContext): Future[Position] = {
+    connection.sendRequest("window_get_cursor", int(id)) {
+      case MsgpackArray(List(MsgpackLong(row), MsgpackLong(col))) ⇒
+        Position(row.toInt, col.toInt)
+    }
+  }
 
 }
