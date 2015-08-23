527e8fdf595e3256914a302993e7b8dddca85d47
Simon Schäfer <mail@antoras.de>
2015-08-23 14:04:30 +0200

    Refactorings in BackendSystem

--- backend/src/main/scala/test/backend/BackendSystem.scala
+++ backend/src/main/scala/test/backend/BackendSystem.scala
@@ -2,7 +2,6 @@ package test.backend
 
 import java.nio.ByteBuffer
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Failure
 import scala.util.Success
 
@@ -18,100 +17,73 @@ import akka.stream.scaladsl.Source
 import nvim._
 import shared.test._
 
+final class NvimAccessor(implicit system: ActorSystem) {
+  val nvim = new Nvim(new Connection("127.0.0.1", 6666))
+
+  implicit val ec = system.dispatcher
+  // we cache window here in order to reduce communication overhead
+  val window = nvim.currentWindow
+
+  def handleTextChange(change: TextChange, sender: String, self: ActorRef) = {
+    println(s"> received: $change")
+    val newCursorPos = for {
+      _ ← nvim.sendInput(change.text)
+      win ← window
+      c ← win.cursor
+    } yield c
+
+    newCursorPos onComplete {
+      case Success(newCursorPos) ⇒
+        // TODO handle multi line cursor positions
+        val resp = TextChangeAnswer(change.bufferRef, newCursorPos.col, newCursorPos.col, change.text)
+        self ! NvimSignal(sender, resp)
+        println(s"> sent: $resp")
+
+      case Failure(f) ⇒
+        system.log.error(f, s"Failed to send response after client request `$change`.")
+    }
+  }
+
+  def handleSelectionChange(change: SelectionChange, sender: String, self: ActorRef) = {
+    println(s"> received: $change")
+    // TODO handle multi line cursor positions
+    val resp = window.flatMap(_.cursor = Position(1, change.start))
+
+    resp onComplete {
+      case Success(_) ⇒
+        val resp = SelectionChangeAnswer(change.bufferRef, change.start, change.start)
+        self ! NvimSignal(sender, resp)
+        println(s"> sent: $resp")
+
+      case Failure(f) ⇒
+        system.log.error(f, s"Failed to send response after client request `$change`.")
+    }
+  }
+
+  def handleControl(control: Control, sender: String, self: ActorRef) = {
+    println(s"> received: $control")
+    val newCursorPos = for {
+      _ ← nvim.sendInput(control.controlSeq)
+      win ← window
+      c ← win.cursor
+    } yield c
+
+    newCursorPos onComplete {
+      case Success(newCursorPos) ⇒
+        val resp = TextChangeAnswer(control.bufferRef, newCursorPos.col, newCursorPos.col, "")
+        self ! NvimSignal(sender, resp)
+        println(s"> sent: $resp")
+
+      case Failure(f) ⇒
+        system.log.error(f, s"Failed to send response after client request `$control`.")
+    }
+  }
+}
+
 final class BackendSystem(implicit system: ActorSystem) {
   import boopickle.Default._
 
-  val actor = system.actorOf(Props(new Actor {
-    val repl = new Repl
-    var clients = Map.empty[String, ActorRef]
-    val nvim = new Nvim(new Connection("127.0.0.1", 6666))
-
-    // we cache window here in order to reduce communication overhead
-    val window = nvim.currentWindow
-
-    override def receive = {
-      case NewClient(subject) ⇒
-        val sender = "client" + clients.size
-        println(s"New client '$sender' seen")
-        subject ! ConnectionSuccessful(sender)
-
-      case ClientReady(sender, subject) ⇒
-        if (clients.contains(sender)) {
-          println(s"'$sender' already exists")
-          // TODO this can only happen when multiple clients try to join at nearly the same moment
-          subject ! ConnectionFailure
-        }
-        else {
-          context.watch(subject)
-          clients += sender → subject
-          println(s"'$sender' joined")
-          subject ! ConnectionSuccessful(sender)
-        }
-
-      case ReceivedMessage(sender, msg) ⇒
-        msg match {
-          case Interpret(id, expr) =>
-            val res = repl.interpret(expr)
-            clients(sender) ! InterpretedResult(id, res)
-
-          case change @ SelectionChange(bufferRef, start, end) ⇒
-            println(s"> received: $change")
-            // TODO handle multi line cursor positions
-            val resp = window.flatMap(_.cursor = Position(1, start))
-
-            resp onComplete {
-              case Success(_) ⇒
-                val resp = SelectionChangeAnswer(bufferRef, start, start)
-                clients(sender) ! resp
-                println(s"> sent: $resp")
-
-              case Failure(f) ⇒
-                system.log.error(f, s"Failed to send response after client request `$change`.")
-            }
-
-          case change @ TextChange(bufferRef, start, end, text) ⇒
-            println(s"> received: $change")
-            val newCursorPos = for {
-              _ ← nvim.sendInput(text)
-              win ← window
-              c ← win.cursor
-            } yield c
-
-            newCursorPos onComplete {
-              case Success(newCursorPos) ⇒
-                // TODO handle multi line cursor positions
-                val resp = TextChangeAnswer(bufferRef, newCursorPos.col, newCursorPos.col, text)
-                clients(sender) ! resp
-                println(s"> sent: $resp")
-
-              case Failure(f) ⇒
-                system.log.error(f, s"Failed to send response after client request `$change`.")
-            }
-
-          case control @ Control(bufferRef, start, end, controlSeq) ⇒
-            println(s"> received: $control")
-            val newCursorPos = for {
-              _ ← nvim.sendInput(controlSeq)
-              win ← window
-              c ← win.cursor
-            } yield c
-
-            newCursorPos onComplete {
-              case Success(newCursorPos) ⇒
-                val resp = TextChangeAnswer(bufferRef, newCursorPos.col, newCursorPos.col, "")
-                clients(sender) ! resp
-                println(s"> sent: $resp")
-
-              case Failure(f) ⇒
-                system.log.error(f, s"Failed to send response after client request `$control`.")
-            }
-        }
-
-      case ClientLeft(sender) ⇒
-        clients -= sender
-        println(s"'$sender' left")
-    }
-  }))
+  private val actor = system.actorOf(Props[MsgActor])
 
   def authFlow(): Flow[ByteBuffer, ByteBuffer, Unit] = {
     val out = Source
@@ -135,8 +107,60 @@ final class BackendSystem(implicit system: ActorSystem) {
   }
 }
 
+final class MsgActor extends Actor {
+  import context.system
+
+  private val repl = new Repl
+  private var clients = Map.empty[String, ActorRef]
+  private val nvim = new NvimAccessor
+
+  override def receive = {
+    case NewClient(subject) ⇒
+      val sender = "client" + clients.size
+      println(s"New client '$sender' seen")
+      subject ! ConnectionSuccessful(sender)
+
+    case ClientReady(sender, subject) ⇒
+      if (clients.contains(sender)) {
+        println(s"'$sender' already exists")
+        // TODO this can only happen when multiple clients try to join at nearly the same moment
+        subject ! ConnectionFailure
+      }
+      else {
+        context.watch(subject)
+        clients += sender → subject
+        println(s"'$sender' joined")
+        subject ! ConnectionSuccessful(sender)
+      }
+
+    case ReceivedMessage(sender, msg) ⇒
+      msg match {
+        case Interpret(id, expr) =>
+          val res = repl.interpret(expr)
+          clients(sender) ! InterpretedResult(id, res)
+
+        case change @ SelectionChange(bufferRef, start, end) ⇒
+          nvim.handleSelectionChange(change, sender, self)
+
+        case change @ TextChange(bufferRef, start, end, text) ⇒
+          nvim.handleTextChange(change, sender, self)
+
+        case control @ Control(bufferRef, start, end, controlSeq) ⇒
+          nvim.handleControl(control, sender, self)
+      }
+
+    case NvimSignal(sender, resp) ⇒
+      clients(sender) ! resp
+
+    case ClientLeft(sender) ⇒
+      clients -= sender
+      println(s"'$sender' left")
+  }
+}
+
 sealed trait Msg
 case class ReceivedMessage(sender: String, req: Request) extends Msg
 case class ClientLeft(sender: String) extends Msg
 case class NewClient(subject: ActorRef) extends Msg
 case class ClientReady(sender: String, subject: ActorRef) extends Msg
+case class NvimSignal(sender: String, resp: Response)
