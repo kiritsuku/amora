af5b25659e47bb06e0fb927f446ad917ca643d04
Simon Schäfer <mail@antoras.de>
2015-09-21 12:56:38 +0200

    Fix latest test in WindowTreeTest

--- backend/src/main/scala/backend/internal/WindowTree.scala
+++ backend/src/main/scala/backend/internal/WindowTree.scala
@@ -27,10 +27,20 @@ object WindowTreeCreator {
   private def mkRows(wins: Seq[WinInfo]): Rows = wins match {
     case Seq(WinInfo(id, _, _)) ⇒
       Rows(Seq(Window(s"window$id")))
-    case WinInfo(id1, _, _) +: WinInfo(id2, _, _) +: xs ⇒
-      val ret = Rows(Seq(Window(s"window$id1"), Window(s"window$id2")))
-      val rows = mkRows(xs)
-      Rows(ret.rows ++ rows.rows)
+    case WinInfo(id1, _, _) +: WinInfo(id2, r1, _) +: xs ⇒
+      if (xs.isEmpty || r1 < xs.head.row) {
+        val ret = Rows(Seq(Window(s"window$id1"), Window(s"window$id2")))
+        val rows = mkRows(xs)
+        Rows(ret.rows ++ rows.rows)
+      }
+      else {
+        val rows = Rows(Seq(Window(s"window$id1")))
+        val ret = mkCols(wins.tail)
+        if (ret._2.isEmpty)
+          Rows(rows.rows :+ ret._1)
+        else
+          ???
+      }
   }
 
   private def mkCols(wins: Seq[WinInfo]): (Columns, Seq[WinInfo]) = wins match {
@@ -38,7 +48,9 @@ object WindowTreeCreator {
       Columns(Seq(Window(s"window$id"))) → Nil
     case WinInfo(id1, _, c1) +: WinInfo(id2, r1, c2) +: xs ⇒
       val cols = Columns(Seq(Window(s"window$id1"), Window(s"window$id2")))
-      if (xs.isEmpty || r1 == xs.head.row) {
+      if (xs.isEmpty)
+        cols → Nil
+      else if (r1 == xs.head.row) {
         val ret = mkCols(xs)
         Columns(cols.columns ++ ret._1.columns) → ret._2
       }
