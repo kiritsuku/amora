d92dad3f08a9fa7eef1150f7775f215fe9c6079d
Simon Schäfer <mail@antoras.de>
2016-03-10 11:45:44 +0100

    Add logic to fill index with data

--- scalac-plugin/src/main/scala/indexer/Indexer.scala
+++ scalac-plugin/src/main/scala/indexer/Indexer.scala
@@ -1,6 +1,19 @@
 package indexer
 
+import java.io.ByteArrayInputStream
+
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+import org.apache.jena.query.Dataset
+import org.apache.jena.query.QueryExecutionFactory
+import org.apache.jena.query.QueryFactory
+
 import org.apache.jena.query.ReadWrite
+import org.apache.jena.query.ResultSetFactory
+import org.apache.jena.query.ResultSetFormatter
+import org.apache.jena.rdf.model.Model
 import org.apache.jena.tdb.TDBFactory
 import org.apache.log4j.ConsoleAppender
 import org.apache.log4j.Level
@@ -21,15 +34,60 @@ object Indexer extends App with LoggerConfig {
     p.substring(0, i+projectName.length)
   }
 
-  val name = "testmodel"
-  val dataset = TDBFactory.createDataset(s"$storageLocation/dataset")
-  dataset.begin(ReadWrite.READ)
-  val model = dataset.getNamedModel(name)
-  dataset.end()
+  val modelName = "testmodel"
+  withDataset(s"$storageLocation/dataset") { dataset ⇒
+    withModel(dataset, modelName)(add)
+    withModel(dataset, modelName)(show)
+  }
 
-  println(model)
+  def show(model: Model) = {
+    val q2 = """SELECT * { ?s ?p ?o }"""
+    val qexec = QueryExecutionFactory.create(QueryFactory.create(q2), model)
+    val r = ResultSetFactory.makeRewindable(qexec.execSelect())
 
-  dataset.close()
+    ResultSetFormatter.out(System.out, r)
+  }
+
+  def add(model: Model) = {
+    val data = s"""
+      @prefix :<$modelName/> .
+      @prefix foaf:<http://xmlns.com/foaf/0.1/> .
+
+      :helloWorld
+        a foaf:String;
+        foaf:name "helloWorld"
+        .
+    """
+    val in = new ByteArrayInputStream(data.getBytes)
+    model.read(in, /* base = */ null, "TURTLE")
+  }
+
+  def withDataset(location: String)(f: Dataset ⇒ Unit) = {
+    val dataset = TDBFactory.createDataset(location)
+    dataset.begin(ReadWrite.WRITE)
+    Try(f(dataset)) match {
+      case Success(v) ⇒
+        dataset.commit()
+      case Failure(f) ⇒
+        f.printStackTrace()
+        dataset.abort()
+    }
+    dataset.end()
+    dataset.close()
+  }
+
+  def withModel(dataset: Dataset, name: String)(f: Model ⇒ Unit) = {
+    val model = dataset.getNamedModel(name)
+    model.begin()
+    Try(f(model)) match {
+      case Success(v) ⇒
+        model.commit()
+      case Failure(f) ⇒
+        f.printStackTrace()
+        model.abort()
+    }
+    model.close()
+  }
 }
 
 trait LoggerConfig {
