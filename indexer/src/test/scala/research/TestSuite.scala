33e8edd9fcd157dd7e557c8288a7222aadab1845
Simon Schäfer <mail@antoras.de>
2016-04-24 13:10:26 +0200

    Add test suite for JavaBytecodeIndexer

--- indexer/src/test/scala/research/TestSuite.scala
+++ indexer/src/test/scala/research/TestSuite.scala
@@ -6,6 +6,7 @@ import org.junit.runners.Suite.SuiteClasses
 
 import research.converter.ClassfileConverterTest
 import research.converter.ScalacConverterTest
+import research.indexer.JavaBytecodeIndexerTest
 import research.indexer.ScalaSourceIndexerTest
 import research.indexer.RegionIndexerTest
 
@@ -14,6 +15,7 @@ import research.indexer.RegionIndexerTest
   classOf[ScalacConverterTest],
   classOf[ScalaSourceIndexerTest],
   classOf[RegionIndexerTest],
-  classOf[ClassfileConverterTest]
+  classOf[ClassfileConverterTest],
+  classOf[JavaBytecodeIndexerTest]
 ))
 class TestSuite
---
+++ indexer/src/test/scala/research/indexer/JavaBytecodeIndexerTest.scala
@@ -0,0 +1,48 @@
+package research
+package indexer
+
+import scala.util._
+import org.junit.Test
+
+class JavaBytecodeIndexerTest {
+
+  import TestUtils._
+
+  case class Data(varName: String, value: String)
+
+  def ask(modelName: String, rawQuery: String, data: (String, String)*): Seq[Data] = {
+    val query = rawQuery.replaceFirst("""\?MODEL\?""", modelName)
+    val res = Indexer.withInMemoryDataset { dataset ⇒
+      Indexer.withModel(dataset, modelName) { model ⇒
+        bytecodeToHierarchy(data) match {
+          case Success(data) ⇒
+            data foreach {
+              case (filename, data) ⇒
+                Indexer.add(modelName, filename, data)(model)
+            }
+          case Failure(f) ⇒
+            throw f
+        }
+
+        if (debugTests) {
+          Indexer.queryResultAsString(modelName, "select * { ?s ?p ?o }", model) foreach println
+          Indexer.queryResultAsString(modelName, query, model) foreach println
+        }
+        Indexer.queryResult(modelName, query, model) { (v, q) ⇒
+          val res = q.get(v)
+          require(res != null, s"The variable `$v` does not exist in the result set.")
+          Data(v, res.toString)
+        }
+      }.flatten
+    }.flatten
+
+    res match {
+      case Success(res) ⇒
+        res.sortBy(d ⇒ (d.varName, d.value))
+      case Failure(f) ⇒
+        throw new RuntimeException("An error happened during the test.", f)
+    }
+  }
+
+  def modelName = "http://test.model/"
+}
