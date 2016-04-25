6a72cf914198ee6f378b3e1f75f5ec71171996fe
Simon Schäfer <mail@antoras.de>
2016-04-25 22:37:23 +0200

    Add log levels to Logger

--- indexer/src/main/scala/research/Logger.scala
+++ indexer/src/main/scala/research/Logger.scala
@@ -3,26 +3,45 @@ package research
 import java.io.PrintWriter
 import java.io.StringWriter
 
+object Logger {
+
+  sealed abstract class LogLevel(val value: Int)
+  case object Info extends LogLevel(1)
+  case object Warning extends LogLevel(2)
+  case object Error extends LogLevel(3)
+}
+
 final class Logger {
+  import Logger._
 
   private val sw = new StringWriter
   private val pw = new PrintWriter(sw)
 
+  private var level: LogLevel = Info
+
   def warning(msg: String): Unit = {
-    pw.print("[warning] ")
-    pw.println(msg)
+    if (level.value >= Warning.value) {
+      pw.print("[warning] ")
+      pw.println(msg)
+    }
   }
 
   def info(msg: String): Unit = {
-    pw.print("[info] ")
-    pw.println(msg)
+    if (level.value >= Info.value) {
+      pw.print("[info] ")
+      pw.println(msg)
+    }
   }
 
   def error(msg: String, t: Throwable): Unit = {
-    pw.print("[error] ")
-    pw.println(msg)
-    t.printStackTrace(pw)
+    if (level.value >= Error.value) {
+      pw.print("[error] ")
+      pw.println(msg)
+      t.printStackTrace(pw)
+    }
   }
 
   def log: String = sw.toString()
+  def logLevel = level
+  def logLevel_=(level: LogLevel) = this.level = level
 }
