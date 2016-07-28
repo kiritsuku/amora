10f871c89ca22a3eb8271c1c5f659e248bbebfb1
Simon Schäfer <mail@antoras.de>
2016-07-28 02:33:35 +0200

    Add test that checks multiple requests at once

--- backend/src/test/scala/backend/indexer/IndexerTest.scala
+++ backend/src/test/scala/backend/indexer/IndexerTest.scala
@@ -1,8 +1,17 @@
 package backend.indexer
 
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+import scala.concurrent.Await
+
+import org.apache.jena.query.ResultSet
+import org.apache.jena.query.ResultSetFactory
+import org.apache.jena.query.ResultSetFormatter
 import org.junit.Test
 
 import akka.http.javadsl.model.headers.RawRequestURI
+import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.model.HttpMethods
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.model.StatusCodes
@@ -36,6 +45,7 @@ class IndexerTest extends TestFrameworkInterface with RouteTest with AkkaLogging
       interface = "localhost"
       port = 7777
       test-mode = true
+      forward-internal-logger-to-akka-logger = true
     }
   """
 
@@ -50,35 +60,91 @@ class IndexerTest extends TestFrameworkInterface with RouteTest with AkkaLogging
 
   @Test
   def jsonld_context_can_be_retrieved(): Unit = {
-    get("http://amora.center/kb/amora/Format/0.1/amora/Format/0.1/schema.jsonld?format=jsonld") ~> route ~> check {
+    testReq(get("http://amora.center/kb/amora/Format/0.1/amora/Format/0.1/schema.jsonld?format=jsonld")) {
       status === StatusCodes.OK
     }
   }
 
   @Test
   def error_for_invalid_format(): Unit = {
-    get("http://amora.center/kb/amora/Format/0.1/amora/Format/0.1/schema.jsonld?format=invalid") ~> route ~> check {
+    testReq(get("http://amora.center/kb/amora/Format/0.1/amora/Format/0.1/schema.jsonld?format=invalid")) {
       status === StatusCodes.BadRequest
     }
   }
 
   @Test
   def error_for_invalid_format_uri(): Unit = {
-    get("http://amora.center/kb/amora/Format/0.1/amora/Format/0.1/invalid.jsonld?format=jsonld") ~> route ~> check {
+    testReq(get("http://amora.center/kb/amora/Format/0.1/amora/Format/0.1/invalid.jsonld?format=jsonld")) {
       status === StatusCodes.NotFound
     }
   }
 
   @Test
   def sparql_post_requests_are_possible(): Unit = {
-    post("http://amora.center/sparql", "query=select * where {?s ?p ?o} limit 3") ~> route ~> check {
+    testReq(post("http://amora.center/sparql", "query=select * where {?s ?p ?o} limit 3", header = Accept(CustomContentTypes.`sparql-results+json`))) {
       status === StatusCodes.OK
     }
   }
 
-  private def post(uri: String, request: String) = {
+  @Test
+  def add_json_post_requests_are_possible(): Unit = {
+    testReq(post("http://amora.center/add-json", """
+      {
+        "tpe":"artifact",
+        "artifacts":[
+          {
+            "organization":"com.freedomotic",
+            "name":"hello-world",
+            "version":"3.0"
+          }
+        ]
+      }
+    """)) {
+      status === StatusCodes.OK
+    }
+    // TODO how to get rid of this timeout here?
+    Thread.sleep(1000)
+    testReq((post("http://amora.center/sparql", """query=
+      prefix kb:<http://amora.center/kb/>
+      select * where {
+        [a kb:Project] kb:name ?name .
+      }
+    """, header = Accept(CustomContentTypes.`sparql-results+json`)))) {
+      val r = respAsResultSet()
+      status === StatusCodes.OK
+      r.next().get("name").asLiteral().getString === "hello-world"
+    }
+  }
+
+  private def testReq(req: HttpRequest)(f: ⇒ Unit) = {
+    req ~> route ~> check {
+      val isJsonResponse = req.header[Accept].flatMap(_.mediaRanges.headOption).exists {
+        case m if m matches CustomContentTypes.`sparql-results+json` ⇒ true
+        case _ ⇒ false
+      }
+      if (debugTests && status == StatusCodes.OK && isJsonResponse) {
+        val r = respAsResultSet()
+        println(resultSetAsString(r))
+      }
+      f
+    }
+  }
+
+  private def resultSetAsString(r: ResultSet): String = {
+    val s = new ByteArrayOutputStream
+    ResultSetFormatter.out(s, r)
+    new String(s.toByteArray(), "UTF-8")
+  }
+
+  private def respAsResultSet(): ResultSet = {
+    val resp = Await.result(response.entity.dataBytes.runFold("")(_ + _.utf8String), timeout.duration)
+    val in = new ByteArrayInputStream(resp.getBytes)
+    ResultSetFactory.makeRewindable(ResultSetFactory.fromJSON(in))
+  }
+
+  private def post(uri: String, request: String, header: HttpHeader*) = {
     val u = Uri(uri)
-    val r = HttpRequest(HttpMethods.POST, u, List(RawRequestURI.create(u.toRelative.toString), Accept(CustomContentTypes.`sparql-results+json`)), request)
+    val r = HttpRequest(HttpMethods.POST, u, List(RawRequestURI.create(u.toRelative.toString)) ++ header, request)
     log.info(s"sending request: $r")
     r
   }
