b65706536b7e325eea8f3c05e299273d15450c9c
Simon Schäfer <mail@antoras.de>
2016-08-04 00:03:12 +0200

    Add first Schema definition as Scala API

---
+++ backend/src/main/scala/backend/schema/Schema.scala
@@ -0,0 +1,31 @@
+package backend.schema
+
+trait Schema
+final case class Project(name: String) extends Schema
+
+object Schema {
+
+  def mkSparqlUpdate(schema: Schema): String = {
+    def mk(o: Schema, sb: StringBuilder, indent: String) = o match {
+      case Project(name) ⇒
+        sb.append(indent)
+        sb.append("<http://amora.center/kb/amora/Project/0.1/")
+        sb.append(name)
+        sb.append("> a <http://amora.center/kb/amora/Schema/0.1/Project/0.1/> .\n")
+        sb.append(indent)
+        sb.append("<http://amora.center/kb/amora/Project/0.1/")
+        sb.append(name)
+        sb.append("> <http://amora.center/kb/amora/Schema/0.1/Project/0.1/name> ")
+        sb.append("\"")
+        sb.append(name)
+        sb.append("\" .\n")
+    }
+
+    val sb = new StringBuilder
+    sb.append("INSERT DATA {\n")
+    mk(schema, sb, "  ")
+    sb.append("}")
+    sb.toString()
+  }
+
+}
