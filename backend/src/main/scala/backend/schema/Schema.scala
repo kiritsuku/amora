69546cc96fc317ee643683f01c13691adc4054bf
Simon Schäfer <mail@antoras.de>
2016-08-07 00:44:44 +0200

    Small refactorings in Schema

--- backend/src/main/scala/backend/schema/Schema.scala
+++ backend/src/main/scala/backend/schema/Schema.scala
@@ -12,7 +12,7 @@ object Schema {
   def mkSparqlUpdate(schemas: Seq[Schema]): String = {
     val sb = new StringBuilder
 
-    def mkShortId(o: Schema): String = o match {
+    def mkShortId(s: Schema): String = s match {
       case Project(name) ⇒
         name
       case Artifact(_, organization, name, version) ⇒
@@ -25,20 +25,20 @@ object Schema {
         s"${mkShortId(owner)}/$name"
     }
 
-    def mkId(o: Schema) = o match {
+    def mkId(s: Schema) = s match {
       case _: Project ⇒
-        s"http://amora.center/kb/amora/Project/0.1/${mkShortId(o)}"
+        s"http://amora.center/kb/amora/Project/0.1/${mkShortId(s)}"
       case _: Artifact ⇒
-        s"http://amora.center/kb/amora/Artifact/0.1/${mkShortId(o)}"
+        s"http://amora.center/kb/amora/Artifact/0.1/${mkShortId(s)}"
       case _: File ⇒
-        s"http://amora.center/kb/amora/File/0.1/${mkShortId(o)}"
+        s"http://amora.center/kb/amora/File/0.1/${mkShortId(s)}"
       case _: Package ⇒
-        s"http://amora.center/kb/amora/Package/0.1/${mkShortId(o)}"
+        s"http://amora.center/kb/amora/Package/0.1/${mkShortId(s)}"
       case _: Class ⇒
-        s"http://amora.center/kb/amora/Class/0.1/${mkShortId(o)}"
+        s"http://amora.center/kb/amora/Class/0.1/${mkShortId(s)}"
     }
 
-    def mkDefn(o: Schema) = o match {
+    def mkDefn(s: Schema) = s match {
       case _: Project ⇒
         s"http://amora.center/kb/amora/Schema/0.1/Project/0.1"
       case _: Artifact ⇒
@@ -51,19 +51,19 @@ object Schema {
         s"http://amora.center/kb/amora/Schema/0.1/Class/0.1"
     }
 
-    def mk(o: Schema, indent: String): String = o match {
+    def mk(s: Schema): String = s match {
       case Project(name) ⇒
-        val id = mkId(o)
-        val defn = mkDefn(o)
+        val id = mkId(s)
+        val defn = mkDefn(s)
         val tpe = "http://schema.org/Text"
         sb.append(s"""|  <$id> a <$defn/> .
                       |  <$id> <$defn/name> "$name"^^<$tpe> .
         |""".stripMargin)
         id
       case Artifact(owner, organization, name, version) ⇒
-        val oid = mk(owner, indent)
-        val id = mkId(o)
-        val defn = mkDefn(o)
+        val oid = mk(owner)
+        val id = mkId(s)
+        val defn = mkDefn(s)
         val tpe = "http://schema.org/Text"
         sb.append(s"""|  <$id> a <$defn/> .
                       |  <$id> <$defn/owner> <$oid> .
@@ -72,32 +72,30 @@ object Schema {
                       |  <$id> <$defn/version> "$version"^^<$tpe> .
         |""".stripMargin)
         id
-      case File(owner @ Artifact(_, organization, name, version), fname, data) ⇒
-        val oid = mk(owner, indent)
-        val id = mkId(o)
-        val defn = mkDefn(o)
+      case File(owner, fname, data) ⇒
+        val oid = mk(owner)
+        val id = mkId(s)
+        val defn = mkDefn(s)
         val tpe = "http://schema.org/Text"
         sb.append(s"""|  <$id> a <$defn/> .
                       |  <$id> <$defn/owner> <$oid> .
                       |  <$id> <$defn/name> "$fname"^^<$tpe> .
         |""".stripMargin)
-        data foreach {
-          mk(_, indent)
-        }
+        data foreach mk
         id
       case Package(name, parent) ⇒
-        val oid = mk(parent, indent)
-        val id = mkId(o)
-        val defn = s"http://amora.center/kb/amora/Schema/0.1/Package/0.1"
+        val oid = mk(parent)
+        val id = mkId(s)
+        val defn = mkDefn(s)
         sb.append(s"""|  <$id> a <$defn/> .
                       |  <$id> <$defn/owner> <$oid> .
                       |  <$id> <$defn/name> "$name"^^<http://schema.org/Text> .
         |""".stripMargin)
         id
       case Class(name, parent) ⇒
-        val oid = mk(parent, indent)
-        val id = mkId(o)
-        val defn = mkDefn(o)
+        val oid = mk(parent)
+        val id = mkId(s)
+        val defn = mkDefn(s)
         sb.append(s"""|  <$id> a <$defn/> .
                       |  <$id> <$defn/owner> <$oid> .
                       |  <$id> <$defn/name> "$name"^^<http://schema.org/Text> .
@@ -106,9 +104,7 @@ object Schema {
     }
 
     sb.append("INSERT DATA {\n")
-    schemas foreach {
-      mk(_, "  ")
-    }
+    schemas foreach mk
     sb.append("}")
     sb.toString()
   }
