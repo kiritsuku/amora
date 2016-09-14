c4726aca8ff5f909a1c5b285927a510107c171ec
Simon Schäfer <mail@antoras.de>
2016-09-14 23:18:23 +0200

    Use new SPARQL update query in JavaBytecodeIndexerActor

--- backend/src/main/scala/amora/backend/actors/JavaBytecodeIndexerActor.scala
+++ backend/src/main/scala/amora/backend/actors/JavaBytecodeIndexerActor.scala
@@ -2,9 +2,10 @@ package amora.backend.actors
 
 import akka.actor.Actor
 import akka.actor.ActorRef
-
 import amora.backend.Logger
 import amora.backend.indexer.JavaBytecodeIndexer
+import amora.backend.schema._
+import amora.converter.protocol._
 
 final class JavaBytecodeIndexerActor(override val indexer: ActorRef, override val logger: Logger)
     extends Actor with DataIndexer  {
@@ -12,18 +13,28 @@ final class JavaBytecodeIndexerActor(override val indexer: ActorRef, override va
   override def receive = {
     case RequestMessage.Files(_, files) ⇒
       runIndexing(sender) {
-        handleJavaBytecode(files, new JavaBytecodeIndexer(logger))
+        handleJavaBytecode(Artifact(Project("testProject"), "o", "n", "v1"), files, new JavaBytecodeIndexer(logger))
       }
     case RequestMessage.GetLogger ⇒
       sender ! logger
   }
 
-  private def handleJavaBytecode(files: Seq[RequestMessage.File], indexer: JavaBytecodeIndexer) = {
+  private def handleJavaBytecode(origin: Schema, files: Seq[RequestMessage.File], indexer: JavaBytecodeIndexer) = {
     val res = indexer.bytecodeToHierarchy(files.map{ f ⇒ f.fileName → f.src }).get
     res foreach {
       case (fileName, hierarchy) ⇒
         logger.info(s"Indexing ${hierarchy.size} entries of file $fileName")
-        indexData(IndexerMessage.File(IndexerMessage.NoOrigin, fileName, hierarchy), s"Error happened while indexing $fileName.")
+        def asSchemaPackage(decl: Hierarchy): Schema = decl match {
+          case Root ⇒ origin
+          case Decl(name, owner) ⇒ Package(name, asSchemaPackage(owner))
+          case _ ⇒ ???
+        }
+        val pkg = hierarchy.collectFirst {
+          case d if d.attachments(Attachment.Package) ⇒ d
+        }
+        val s = pkg.map(asSchemaPackage).map(pkg ⇒ File(pkg, fileName)).getOrElse(File(origin, fileName))
+        sparqlUpdate(Schema.mkSparqlUpdate(Seq(s)), s"Error happened while indexing $fileName.")
+        sparqlUpdate(HierarchySchema.mkSparqlUpdate(s, hierarchy), s"Error happened while indexing $fileName.")
     }
   }
 
