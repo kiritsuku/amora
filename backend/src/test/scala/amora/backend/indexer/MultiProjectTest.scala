5354859e3a0e83673e2f4eb5536be39b4b3a5632
Simon Schäfer <mail@antoras.de>
2016-10-28 17:50:33 +0200

    Improve tests in MultiProjectTest

--- backend/src/test/scala/amora/backend/indexer/MultiProjectTest.scala
+++ backend/src/test/scala/amora/backend/indexer/MultiProjectTest.scala
@@ -27,15 +27,39 @@ class MultiProjectTest extends RestApiTest {
   @Test
   def add_multiple_projects_with_artifacts(): Unit = {
     val a1 = Artifact(Project("p1"), "o1", "n1", "v1")
-    val a2 = Artifact(Project("p2"), "o2", "n2", "v2")
-    turtleRequest(Schema.mkTurtleString(Seq(a1, a2)))
+    val a2 = Artifact(Project("p1"), "o1", "n1", "v2")
+    val a3 = Artifact(Project("p2"), "o2", "n2", "v1")
+    val a4 = Artifact(Project("p2"), "o2", "n2", "v2")
+    turtleRequest(Schema.mkTurtleString(Seq(a1, a2, a3, a4)))
     sparqlRequest("""
       prefix a:<http://amora.center/kb/amora/Schema/0.1/Artifact/0.1/>
       select * where {
-        [a a:] a:name ?name .
+        [a a:] a:name ?name ; a:version ?version .
       }
+      order by ?name ?version
     """) === Seq(
-        Seq(Data("name", "n1")),
-        Seq(Data("name", "n2")))
+        Seq(Data("name", "n1"), Data("version", "v1")),
+        Seq(Data("name", "n1"), Data("version", "v2")),
+        Seq(Data("name", "n2"), Data("version", "v1")),
+        Seq(Data("name", "n2"), Data("version", "v2")))
+  }
+
+  @Test
+  def artifacts_can_be_searched_by_project(): Unit = {
+    val a1 = Artifact(Project("p1"), "o1", "n1", "v1")
+    val a2 = Artifact(Project("p1"), "o1", "n1", "v2")
+    val a3 = Artifact(Project("p2"), "o2", "n2", "v1")
+    val a4 = Artifact(Project("p2"), "o2", "n2", "v2")
+    turtleRequest(Schema.mkTurtleString(Seq(a1, a2, a3, a4)))
+    sparqlRequest("""
+      prefix p:<http://amora.center/kb/amora/Schema/0.1/Project/0.1/>
+      prefix a:<http://amora.center/kb/amora/Schema/0.1/Artifact/0.1/>
+      select * where {
+        [a a:] a:owner [a p: ; p:name "p1"] ; a:name ?name ; a:version ?version .
+      }
+      order by ?name ?version
+    """) === Seq(
+        Seq(Data("name", "n1"), Data("version", "v1")),
+        Seq(Data("name", "n1"), Data("version", "v2")))
   }
 }
