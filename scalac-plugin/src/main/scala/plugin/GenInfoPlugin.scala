18b40531758721fa6d3c5455e64ee505a81c1951
Simon Schäfer <mail@antoras.de>
2017-03-11 13:20:26 +0100

    Better logging in scalac plugin

--- scalac-plugin/src/main/scala/plugin/GenInfoPlugin.scala
+++ scalac-plugin/src/main/scala/plugin/GenInfoPlugin.scala
@@ -1,5 +1,7 @@
 package plugin
 
+import java.io.PrintWriter
+import java.io.StringWriter
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -27,32 +29,38 @@ class GenInfoComponent(override val global: Global) extends PluginComponent {
   import global._
 
   override def newPhase(prev: Phase): Phase = new Phase(prev) {
-    override def run() = {
-      def mkHierarchy(t: global.Tree) = {
-        new ScalacConverter[global.type](global).convert(t) match {
-          case Success(res) ⇒
-            res
-          case Failure(f) ⇒
-            f.printStackTrace()
-            Nil
-        }
-      }
+    override def run(): Unit = {
+      if (currentRun.units.isEmpty)
+        return
+
       val outputDir = global.settings.outputDirs.getSingleOutput.getOrElse {
         throw new IllegalStateException("scalac has no output directory configured, therefore Amora compiler plugin can't store its data.")
       }.file.getAbsolutePath
 
-      val files = currentRun.units map { u ⇒
-        val is = mkHierarchy(u.body)
+      println("Amora compiler plugin writes databases:")
+      currentRun.units foreach { u ⇒
+        import scala.collection.JavaConverters._
         val file = u.source.file.file
         val fileName = file.getAbsolutePath.replace('/', '%')
         val filePath = s"$outputDir/$fileName.amoradb"
+        val hierarchy = new ScalacConverter[global.type](global).convert(u.body)
 
-        val data = Schema.mkTurtleUpdate(File(Artifact(Project("p"), "o", "n", "v1"), file.getName), is)
-        import scala.collection.JavaConverters._
+        val data = hierarchy match {
+          case Success(res) ⇒
+            Schema.mkTurtleUpdate(File(Artifact(Project("p"), "o", "n", "v1"), file.getName), res)
+          case Failure(f) ⇒
+            val sw = new StringWriter
+            val pw = new PrintWriter(sw)
+            f.printStackTrace(pw)
+            sw.toString()
+        }
+
         Files.write(Paths.get(filePath), Seq(data).asJava, Charset.forName("UTF-8"))
-        filePath
+        if (hierarchy.isSuccess)
+          println(s"- success:     $filePath")
+        else
+          println(s"- with errors: $filePath")
       }
-      println("Amora compiler plugin wrote files:\n" + files.mkString("- ", "\n- ", ""))
     }
     override def name = "GenInfoPhase"
   }
