309663ae9ed865e462cfb01e1e6da9ee1dfe7abf
Simon Schäfer <mail@antoras.de>
2017-04-17 01:07:51 +0200

    Read config file on startup by passing it as parameter
    The default config file that is loaded is now ~/amora.conf. Its
    fall-back is the src/main/resources/application.conf file. A config file
    can be passed as a parameter on startup of the server. The actor system
    uses the combined configs if necessary instead of just the
    application.conf.

--- backend/src/main/scala/amora/backend/Main.scala
+++ backend/src/main/scala/amora/backend/Main.scala
@@ -8,6 +8,7 @@ import org.apache.log4j.Level
 import org.apache.log4j.LogManager
 import org.apache.log4j.PatternLayout
 
+import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 
 import akka.actor.ActorSystem
@@ -24,38 +25,53 @@ import amora.backend.actors.UnhandledMessagesActor
  * We need to depend on a log4j root logger because Jena uses log4j internally.
  * TODO Figure out how to unify Akka logging and Jena logging
  */
-object Main extends App with AkkaLogging with Log4jRootLogging {
-  override implicit val system = ActorSystem("backend")
-  implicit val materializer = ActorMaterializer()
-  import system.dispatcher
+object Main extends AkkaLogging with Log4jRootLogging {
+  private var _config: Config = _
+  private var _system: ActorSystem = _
 
-  val config = {
-    val f = new java.io.File(s"${System.getProperty("user.home")}/application.conf")
-    if (f.exists()) {
-      log.info(s"Reading config file `$f`")
-      ConfigFactory.parseFile(f).withFallback(system.settings.config)
+  def main(args: Array[String]) = {
+    val path =
+      if (args.length == 1)
+        args.head
+      else
+        s"${System.getProperty("user.home")}/amora.conf"
+    val f = new java.io.File(path)
+
+    _config =
+      if (f.exists())
+        ConfigFactory.parseFile(f).resolve().withFallback(ConfigFactory.load())
+      else
+        ConfigFactory.load()
+
+    _system = ActorSystem("amora", _config)
+    implicit val system = _system
+
+    if (f.exists())
+      log.info(s"Succesfully read config file `$f`.")
+
+    system.eventStream.subscribe(
+        system.actorOf(Props[UnhandledMessagesActor], "unhandled-messages"),
+        classOf[UnhandledMessage])
+
+    implicit val materializer = ActorMaterializer()
+    val service = new WebService
+    val binding = Http().bindAndHandle(service.route, interface, port)
+
+    import system.dispatcher
+    binding.onComplete {
+      case Success(binding) ⇒
+        val addr = binding.localAddress
+        log.info(s"Server is listening on ${addr.getHostName}:${addr.getPort}")
+      case Failure(e) ⇒
+        log.error(e, "Failed to start server")
+        system.terminate()
     }
-    else
-      system.settings.config
   }
-  val interface = config.getString("app.interface")
-  val port = config.getInt("app.port")
 
-  system.eventStream.subscribe(
-      system.actorOf(Props[UnhandledMessagesActor], "unhandled-messages"),
-      classOf[UnhandledMessage])
-
-  val service = new WebService
-
-  val binding = Http().bindAndHandle(service.route, interface, port)
-  binding.onComplete {
-    case Success(binding) ⇒
-      val addr = binding.localAddress
-      log.info(s"Server is listening on ${addr.getHostName}:${addr.getPort}")
-    case Failure(e) ⇒
-      log.error(e, "Failed to start server")
-      system.terminate()
-  }
+  def system = _system
+  def config = _config
+  def interface = config.getString("app.interface")
+  def port = config.getInt("app.port")
 }
 
 trait Log4jLoggingConfig {
